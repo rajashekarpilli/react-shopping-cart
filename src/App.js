8b800c59914967a97fdf958ec4d1f8f40ee3d60a
shekaruid <shekaruid@gmail.com>
2020-10-06 20:04:52 +0530

    Divided into parts.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import React from 'react';
 function App() {
   return (
     <div className="grid-container">
+
+      {/* Header */}
       <header className="grid-header">
         <a href="/">React Shopping Cart</a>
       </header>
+
+      {/* Main */}
       <main>
         Products List
       </main>
+
+      {/* Footer */}
       <footer>
         All right is reserved.
       </footer>
