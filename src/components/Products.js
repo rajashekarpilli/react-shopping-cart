ee7aaa14b18582e075650730f8d664e83f16c8af
shekaruid <shekaruid@gmail.com>
2020-10-07 15:04:19 +0530

    cart component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import formatCurrency from '../util'
 
-const Products = ({ products }) => {
+const Products = ({ products, addToCart }) => {
     return (
         <div className="products">
             <ul>
@@ -14,7 +14,7 @@ const Products = ({ products }) => {
                             </a>
                             <div className="product-price">
                                 <div>{formatCurrency(product.price)}</div>
-                                <button className="button primary">Add To Cart</button>
+                                <button className="button primary" onClick={() => addToCart(product)}>Add To Cart</button>
                             </div>
                         </div>
                     </li>
