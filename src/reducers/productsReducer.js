36867905a404cea08e2189786e5a0ee141f87d6f
shekaruid <shekaruid@gmail.com>
2020-10-11 15:32:37 +0530

    add redux products

---
+++ src/reducers/productsReducer.js
@@ -0,0 +1,20 @@
+import * as actions from '../actions/productsActions'
+
+export const initialState = {
+    products: [],
+    loading: false,
+    hasErrors: false,
+}
+
+export default function productsReducer(state = initialState, action) {
+    switch (action.type) {
+        case actions.GET_PRODUCTS:
+            return { ...state, loading: true }
+        case actions.GET_PRODUCTS_SUCCESS:
+            return { ...state, products: action.payload, loading: false, hasErrors: false }
+        case actions.GET_PRODUCTS_FAILURE:
+            return { ...state, loading: false, hasErrors: true }
+        default:
+            return state
+    }
+}
